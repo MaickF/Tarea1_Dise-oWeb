bf64965679563909d56896b7d8b1c16273cc6364
Fabian <superfabian58@gmail.com>
2023-02-13 15:51:54 -0600

    Registro y log in

--- Login/database/migrations/2014_10_12_000000_create_users_table.php
+++ Login/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->int('inicios_sesion');
             $table->rememberToken();
             $table->timestamps();
         });
